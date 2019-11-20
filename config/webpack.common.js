77487039bae1d64da9468f0827db152551e2ebb5
Proxn <gzproxn@gmail.com>
2019-11-21 00:00:27 +0100

    webpack config

---
+++ config/webpack.common.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    main: './src/js/app.js'
+  },
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[hash].[ext]',
+            outputPath: 'img'
+          }
+        }
+      }
+    ]
+  }
+};
---
+++ config/webpack.prod.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const common = require('./webpack.common');
+const merge = require('webpack-merge');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = merge(common, {
+  mode: 'production',
+  output: {
+    filename: 'js/[name].[contentHash].js',
+    path: path.resolve(__dirname, '../', 'dist')
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[contentHash].css'
+    }),
+    new CleanWebpackPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      }
+    ]
+  }
+});
